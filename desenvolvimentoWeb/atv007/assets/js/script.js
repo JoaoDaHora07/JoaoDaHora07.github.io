22ff9679ce3f9c0d4fb12d4d6097accba9eff67f
chrissnts <christiandossantos125@gmail.com>
2025-05-13 15:17:11 -0300

    update 0.11.6

--- desenvolvimentoWeb/atv007/assets/js/script.js
+++ desenvolvimentoWeb/atv007/assets/js/script.js
@@ -8,7 +8,14 @@ let pausando = false;
 const velocidadeTiro = 10;
 const intervalo = 30;
 const shots = [];
+let life = 3;
+let score = 0;
 
+
+window.onload = function () {
+    contar();
+    moverNaveInimiga();
+};
 
 function add() {
 
@@ -64,9 +71,11 @@ document.addEventListener("keydown", function (event) {
         if (contando) {
             pararContar();
             document.getElementById("paused").style.display = 'flex';
+           
         } else {
             contar();
             document.getElementById("paused").style.display = 'none';
+            
         }
     }
 });
@@ -96,20 +105,21 @@ document.addEventListener('keydown', function (event) {
 
 
 function shot() {
+    if (pausando) return;
     const shot = document.createElement('div');
     shot.classList.add('shot');
 
-    
+
     const contentRect = document.querySelector('#content').getBoundingClientRect();
-    const naveRect = nave.getBoundingClientRect(); 
+    const naveRect = nave.getBoundingClientRect();
 
     console.log(contentRect.left);
     console.log(naveRect.left);
     console.log(naveRect.left - contentRect.left + naveRect.width / 2 - 5);
     shot.style.left = `${(naveRect.left - contentRect.left + naveRect.width / 2 - 5) + 293}px`;
-    shot.style.top = `${naveRect.top - contentRect.top - 20}px`; 
+    shot.style.top = `${naveRect.top - contentRect.top - 20}px`;
 
-    
+
     shot.style.position = 'absolute';
     document.querySelector('#content').appendChild(shot);
     shots.push({
@@ -123,6 +133,8 @@ function shot() {
 
 
 function moveShots() {
+
+    if (pausando) return;
     for (let i = shots.length - 1; i >= 0; i--) {
         const shot = shots[i];
         const el = shot.element;
@@ -130,22 +142,57 @@ function moveShots() {
 
         if (shot.direcao === 'subindo') {
             top -= velocidadeTiro;
-            if (top <= 0) {
-                
-                shot.direcao = 'descendo';
-            }
-        } else {
-            top += velocidadeTiro;
-            if (top > window.innerHeight - 35) {
+            if (top < -20) {
                 el.remove();
                 shots.splice(i, 1);
-                continue;
             }
-        }
 
-        el.style.top = `${top}px`;
+            el.style.top = `${top}px`;
+        }
     }
 }
 
-
 setInterval(moveShots, intervalo);
+
+
+function moverNaveInimiga() {
+    const inimiga = document.getElementById("containerEnemies");
+    const amiga = document.getElementById("naveAmiga");
+
+    intervaloInimigo = setInterval(() => {
+        if (pausando) return; 
+
+        const inimigaTop = parseInt(getComputedStyle(inimiga).top || 0);
+        inimiga.style.top = (inimigaTop + 5) + "px";
+
+        const inimigaRect = inimiga.getBoundingClientRect();
+        const amigaRect = amiga.getBoundingClientRect();
+
+        if (
+            inimigaRect.bottom >= amigaRect.top &&
+            inimigaRect.left < amigaRect.right &&
+            inimigaRect.right > amigaRect.left
+        ) {
+            clearInterval(intervaloInimigo);
+            life = life - 1;
+
+            if (life == 0) {
+                console.log("Game Over");
+                document.getElementById("paused").style.display = "flex";
+                document.getElementById("paused").innerHTML = "<p>GAME OVER</p>";
+
+                clearInterval(intervaloInimigo);
+                clearInterval(id); // para o relógio
+                pausando = true;
+
+                setTimeout(() => {
+                    location.reload();
+                }, 3000);
+            } else {
+                document.querySelector("#life").textContent = "LIFE:" + life;
+                inimiga.style.top = 0;
+                moverNaveInimiga();
+            }
+        }
+    }, 30);
+}
