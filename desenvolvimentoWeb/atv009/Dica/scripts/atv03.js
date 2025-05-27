858c69559142aa7623cb73e4c7ae2ca66e8b1652
ArthurDgx <arthurrdgx@gmail.com>
2025-05-27 16:38:42 -0300

    hello hello

---
+++ desenvolvimentoWeb/atv009/Dica/scripts/atv03.js
@@ -0,0 +1,21 @@
+
+function ex03() {
+    
+    const form = document.querySelector('#form03')
+    const input = form.querySelector('input[name="in_03"]').value
+    alert(input)
+    form.reset()
+}
+
+function resolve03(...numbers) {
+    
+}
+
+function isEven(val) {
+
+    if(val % 2 == 0){
+
+    }
+}
+
+
