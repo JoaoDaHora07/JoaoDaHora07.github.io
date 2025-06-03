8556852cd663b877d9f59888e1bfdd3cedfab949
Maria Eduarda <mariah.bsc18@gmail.com>
2025-06-03 13:29:10 -0300

    yessir

--- desenvolvimentoWeb/atv009/Dica/scripts/atv03.js
+++ desenvolvimentoWeb/atv009/Dica/scripts/atv03.js
@@ -1,21 +1,19 @@
-
 function ex03() {
-    
-    const form = document.querySelector('#form03')
-    const input = form.querySelector('input[name="in_03"]').value
-    alert(input)
-    form.reset()
+    const form = document.querySelector('#form03');
+    const input = form.querySelector('input[name="in_03"]').value;
+    const numbers = input.trim().split(" ").map(Number);
+    document.getElementById('output').innerHTML = resolve03(numbers);
+    form.reset();
 }
 
-function resolve03(...numbers) {
-    
+function resolve03(numbers) {
+    const evenOrOdd = numbers.map(function (item) {
+        return isEven(item);
+    });
+
+    return evenOrOdd.join("<br>");
 }
 
 function isEven(val) {
-
-    if(val % 2 == 0){
-
-    }
+    return val % 2 === 0 ? "PAR" : "ÍMPAR";
 }
-
-
